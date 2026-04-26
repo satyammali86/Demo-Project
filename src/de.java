72ffd4fd05da2d6dd06fbf14613e259b7b1359e4
student <malisatyam86@gmail.com>
2026-04-26 21:09:19 +0530

    save the changes

--- src/de.java
+++ src/de.java
@@ -1,7 +1,11 @@
 public class de {
+    public static void hello(){
+        System.out.println("Hello everyone");
+    }
     public static void main(String[] args)
     {
-        System.out.println("Hello");
+        System.out.println("Hello,");
+        hello();
     }
 
 }
